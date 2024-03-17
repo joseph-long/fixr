7654ac74a69d674c9ab19d5b1fef731315b851e3
Joseph Long <github@joseph-long.com>
2024-03-17 10:34:06 -0300

    Initial commit

---
+++ build_xrif_and_generate_bindings.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+git submodule update --init --recursive
+cd xrif
+mkdir -p build
+cd build
+if [[ $(uname) == "Darwin" ]]; then
+    extraDefines='-DCMAKE_C_FLAGS="-DXRIF_NO_OMP"'
+    libExtension=dylib
+else
+    extraDefines=''
+    libExtension=so
+fi
+# build xrif
+cmake .. $extraDefines || exit 1
+make -j $(getconf _NPROCESSORS_ONLN) || exit 1
+cd ../../src/fixr/
+
+# copy compiled artifact into Python module for distribution
+cp ../../xrif/build/src/libxrif.$libExtension ./
+
+# generate bindings
+clang2py \
+    -k cdefstum \
+    -l ../../xrif/build/src/libxrif.$libExtension ../../xrif/src/xrif.h > ./_xrif_rest.py \
+    || exit 1
+
+# massage codegen output
+sed -i '' "s,'\.\./xrif/build/src/libxrif.$libExtension',bundled_lib_path," ./_xrif_rest.py
+echo "import os.path" > ./_xrif.py
+echo "bundled_lib_path = libname = os.path.abspath(os.path.join(os.path.dirname(__file__), \"libxrif.$libExtension\"))" >> ./_xrif.py
+cat ./_xrif_rest.py >> ./_xrif.py
+rm ./_xrif_rest.py
+
+# smoke test to ensure it will import and load the C library
+python -c 'import _xrif' || exit 1
