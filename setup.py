228e4c6a30258b297f0fdf3ee8547aeb221d2863
Joseph Long <github@joseph-long.com>
2024-03-21 23:32:53 -0700

    what if ctypes extension with none abi

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
-from setuptools import Extension, setup
+from setuptools import setup, Extension
+from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+
+class bdist_wheel_abi_none(_bdist_wheel):
+    def finalize_options(self):
+        _bdist_wheel.finalize_options(self)
+        self.root_is_pure = False
+
+    def get_tag(self):
+        python, abi, plat = _bdist_wheel.get_tag(self)
+        return "py3", "none", plat
 
 setup(
     ext_modules=[
@@ -6,5 +16,6 @@ setup(
             name="fixr._xrif",
             sources=[],  # empty list because we compile this separately
         ),
-    ]
+    ],
+    cmdclass={"bdist_wheel": bdist_wheel_abi_none},
 )
